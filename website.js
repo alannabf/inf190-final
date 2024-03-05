52dc4f9183a043d4c593cdbc77d8e8649183b12d
zarashams12481 <shamsz@uci.edu>
2024-03-05 13:01:36 -0800

    waves to json

--- website.js
+++ website.js
@@ -1,20 +1,38 @@
-
-// Play/pause button
 const playPauseButton = document.getElementById('playPauseButton');
-const audio = new Audio() //enter a specific song in parenthesis; need to figure out 
+const audio = document.getElementById('audio-player');
+const wavesurfer = WaveSurfer.create({
+    container: '#waveform', 
+    waveColor: 'rgb(200, 0, 200)',
+    progressColor: 'rgb(100, 0, 100)',
+    backend: 'MediaElement',
+    mediaControls: true,
+    mediaType: 'audio',
+    responsive: true
+});
 
 let isPlaying = false;
+
+
+
+//play pause for waves
 
 function togglePlayPause() {
     if (isPlaying) {
         playPauseButton.textContent = "Play";
-        audio.play();
+        audio.pause(); // Pause the audio
+        wavesurfer.pause(); // Pause the WaveSurfer visualization
     } else {
         playPauseButton.textContent = "Pause";
-        audio.pause();
+        audio.play(); // Play the audio
+        wavesurfer.play(); // Play the WaveSurfer visualization
     }
     // Toggle the playing state
     isPlaying = !isPlaying;
 }
 
 playPauseButton.addEventListener('click', togglePlayPause);
+
+wavesurfer.load('sounds/kalachashma_drums.mp3'); // Load your audio file
+wavesurfer.on('ready', function () {
+    wavesurfer.play(); // Auto-play the waveform visualization when ready
+});
