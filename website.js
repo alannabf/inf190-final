f62793b1bc19a2737555ce4aa72fe8e5c42cc49f
alannabf <92060277+alannabf@users.noreply.github.com>
2024-03-04 18:18:33 -0800

    dropdowns

---
+++ website.js
@@ -0,0 +1,10 @@
+
+<script>
+
+document.addEventListener("", function() {
+    // This function will be called when the DOM is fully loaded
+    var demoElement = document.getElementById("demo");
+    demoElement.innerHTML = "Hello, JavaScript!";
+};
+
+</script>
